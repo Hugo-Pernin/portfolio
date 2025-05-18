3403fc4d2e329c4af9d99654936636cb1e8b7121
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-18 17:30:27 +0200

    Delete test from script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-import { Technology } from "./Technology.js";
 console.log("Hello world!");
-var test = document.getElementById("test");
-var webstorm = new Technology("WebStorm", "https://resources.jetbrains.com/storage/products/company/brand/logos/WebStorm_icon.svg");
-test.appendChild(webstorm.getBigCard());
-test.appendChild(webstorm.getSmallCard());
-console.log("test2");
