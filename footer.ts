eba4126e87f43f732f8fb01de1b52f6f05e33e27
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-21 19:53:43 +0200

    Add GitHub link in the footer

--- footer.ts
+++ footer.ts
@@ -4,14 +4,28 @@ let text: HTMLParagraphElement = document.createElement("p");
 text.innerHTML = "© Hugo Pernin 2025<br>Tous droits réservés.";
 footer.appendChild(text);
 
-let link: HTMLAnchorElement = document.createElement("a");
-link.href = "https://www.linkedin.com/in/hugo-pernin/";
-link.target = "_blank";
+let links: HTMLDivElement = document.createElement("div");
+links.id = "footer-links";
+footer.appendChild(links);
 
-let image: HTMLImageElement = document.createElement("img");
-image.src = "img/linkedin.png";
-image.alt = "LinkedIn";
-image.title = "LinkedIn";
+let github: HTMLAnchorElement = document.createElement("a");
+github.href = "https://github.com/Hugo-Pernin";
+github.target = "_blank";
+links.appendChild(github);
 
-link.appendChild(image);
-footer.appendChild(link);
+let githubImage: HTMLImageElement = document.createElement("img");
+githubImage.src = "img/github.svg";
+githubImage.alt = "GitHub";
+githubImage.title = "GitHub";
+github.appendChild(githubImage);
+
+let linkedin: HTMLAnchorElement = document.createElement("a");
+linkedin.href = "https://www.linkedin.com/in/hugo-pernin/";
+linkedin.target = "_blank";
+links.appendChild(linkedin);
+
+let linkedinImage: HTMLImageElement = document.createElement("img");
+linkedinImage.src = "img/linkedin.png";
+linkedinImage.alt = "LinkedIn";
+linkedinImage.title = "LinkedIn";
+linkedin.appendChild(linkedinImage);
