8efd8900802167ba2dd2d093d152e691445f00c9
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-23 20:21:16 +0200

    Create ProjectCategory.ts

---
+++ ProjectCategory.ts
@@ -0,0 +1,48 @@
+import {Project} from "./Project.js";
+
+/**
+ * A category of projects
+ */
+export class ProjectCategory {
+    private name: string;
+    private projects: Project[];
+
+    /**
+     * Creates a category of projects
+     * @param name name of the category
+     */
+    public constructor(name: string) {
+        this.name = name;
+        this.projects = [];
+    }
+
+    /**
+     * Adds projects to the projects list
+     * @param projects projects to add
+     */
+    public addProjects(projects: Project[]) {
+        this.projects = this.projects.concat(projects);
+    }
+
+    /**
+     * Gets the card of the category which displays its name, and its projects
+     * @return the card
+     */
+    public getCard(): HTMLDivElement {
+        let card: HTMLDivElement = document.createElement("div");
+        card.classList.add("project-category-card");
+
+        let name: HTMLHeadingElement = document.createElement("h2");
+        name.innerText = this.name;
+        card.appendChild(name);
+
+        let projects: HTMLDivElement = document.createElement("div");
+        for (let project of this.projects) {
+            projects.appendChild(project.getCard());
+        }
+
+        card.appendChild(projects);
+
+        return card;
+    }
+}
