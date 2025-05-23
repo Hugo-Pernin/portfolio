c0380d21b7ce12d928566a85301b303aefe855f1
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-23 20:22:18 +0200

    Use project categories instead of projects in projets.ts

--- js/projets.js
+++ js/projets.js
@@ -1,8 +1,8 @@
 import { Loader } from "./Loader.js";
 var loader = Loader.getInstance();
-var projectsList = loader.getProjects();
-var projectsDiv = document.getElementById("liste-projets");
-for (var _i = 0, projectsList_1 = projectsList; _i < projectsList_1.length; _i++) {
-    var project = projectsList_1[_i];
-    projectsDiv.appendChild(project.getCard());
+var categories = loader.getProjectCategories();
+var categoriesDiv = document.getElementById("liste-projets");
+for (var _i = 0, categories_1 = categories; _i < categories_1.length; _i++) {
+    var category = categories_1[_i];
+    categoriesDiv.appendChild(category.getCard());
 }
--- projets.ts
+++ projets.ts
@@ -1,12 +1,12 @@
 import {Loader} from "./Loader.js";
-import {Project} from "./Project.js";
+import {ProjectCategory} from "./ProjectCategory.js";
 
 let loader: Loader = Loader.getInstance()
 
-let projectsList: Project[] = loader.getProjects();
+let categories: ProjectCategory[] = loader.getProjectCategories();
 
-let projectsDiv: HTMLElement = document.getElementById("liste-projets");
+let categoriesDiv: HTMLElement = document.getElementById("liste-projets");
 
-for (let project of projectsList) {
-    projectsDiv.appendChild(project.getCard());
+for (let category of categories) {
+    categoriesDiv.appendChild(category.getCard());
 }
