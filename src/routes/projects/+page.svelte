1d60f66000655c2e05255874e6e96b627c83aebb
Hugo-Pernin <hugopernin71@gmail.com>
2026-01-31 17:55:59 +0100

    Use ProjectCategoryCard component

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import projects from '$lib/data/projects.json';
-	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import ProjectCategoryCard from '$lib/components/ProjectCategoryCard.svelte';
 </script>
 
 <svelte:head>
@@ -10,71 +10,5 @@
 <h1>Mes projets</h1>
 
 {#each projects as category (category)}
-	<div class="project-category-card">
-		<h2>{category.categoryName}</h2>
-		<div class="project-category">
-			{#each category.projects as project (project)}
-				<ProjectCard {project} />
-			{/each}
-		</div>
-	</div>
+	<ProjectCategoryCard {category} />
 {/each}
-
-<style>
-	.project-category-card {
-		margin-top: 4%;
-	}
-
-	.project-category {
-		display: flex;
-		flex-wrap: wrap;
-	}
-
-	.project-card {
-		border-radius: 10px;
-		padding: 1%;
-		margin: 1%;
-		background-color: #f0f0f0;
-		width: 46%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-	}
-
-	.name-and-technologies {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	.technologies {
-		display: flex;
-		align-items: center;
-		justify-content: flex-end;
-	}
-
-	.project-card p {
-		margin: 1% 0;
-		white-space: pre-line;
-	}
-
-	.links-and-year {
-		display: flex;
-	}
-
-	.links {
-		flex: 1;
-	}
-
-	.year {
-		flex: 0;
-		display: flex;
-		align-items: end;
-	}
-
-	@media only screen and (max-width: 600px) {
-		.project-card {
-			width: 100%;
-		}
-	}
-</style>
