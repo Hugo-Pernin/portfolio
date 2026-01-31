e46432206039fe3472dc291fe8bbf1000e976597
Hugo-Pernin <hugopernin71@gmail.com>
2026-01-31 18:56:08 +0100

    Use TechnologyCategoryCard component

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import skills from '$lib/data/skills.json';
 	import technologyCategories from '$lib/data/technologyCategories.json';
-	import technologies from '$lib/data/technologies.json';
-	import TechnologyBigCard from '$lib/components/TechnologyBigCard.svelte';
-
-	const technologiesMap = Object.fromEntries(
-		technologies.map((technology) => [technology.name, technology])
-	);
+	import TechnologyCategoryCard from '$lib/components/TechnologyCategoryCard.svelte';
 </script>
 
 <svelte:head>
@@ -30,14 +25,7 @@
 	<h2 class="skill-category-title">Langages et logiciels</h2>
 	<div id="langages-et-logiciels" class="skill-category">
 		{#each technologyCategories as category (category)}
-			<div class="technology-category-card">
-				<h3>{category.categoryName}</h3>
-				<div>
-					{#each category.technologies as technology (technology)}
-						<TechnologyBigCard technology={technologiesMap[technology]} />
-					{/each}
-				</div>
-			</div>
+			<TechnologyCategoryCard {category} />
 		{/each}
 	</div>
 </div>
@@ -59,46 +47,5 @@
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
-	}
-
-	.technology-category-card {
-		margin: 2% 1% 0;
-		width: 23%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-
-	.technology-category-card > h3 {
-		text-align: center;
-		margin-bottom: 1%;
-	}
-
-	.technology-category-card > div {
-		width: 100%;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
-
-	.technology-big-card {
-		border: 1px solid rgba(0, 0, 0, 0.1);
-		background-color: #f0f0f0;
-		border-radius: 20px;
-		margin: 1%;
-		padding: 0.75% 2.25%;
-		display: inline-flex;
-		align-items: center;
-	}
-
-	.technology-big-card img {
-		height: 25px;
-		margin-right: 5px;
-	}
-
-	@media only screen and (max-width: 600px) {
-		.technology-category-card {
-			width: 100%;
-		}
 	}
 </style>
