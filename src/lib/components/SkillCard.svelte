ae9fa77d0b3ed47c0c93f12851e617ea95ca783f
Hugo-Pernin <hugopernin71@gmail.com>
2026-01-31 19:06:24 +0100

    Create SkillCard component

---
+++ src/lib/components/SkillCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Props {
+		skill: string;
+	}
+
+	let props: Props = $props();
+</script>
+
+<p class="skill-card">{props.skill}</p>
+
+<style>
+	.skill-card {
+		border-radius: 10px;
+		padding: 1%;
+		margin: 1%;
+		background-color: #f0f0f0;
+		display: inline-block;
+	}
+</style>
