d32d0bb5df37249bac8d32216d87cde87878e1f9
Hugo-Pernin <hugopernin71@gmail.com>
2026-02-01 15:56:42 +0100

    Create IndexButton component

---
+++ src/lib/components/IndexButton.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	interface Props {
+		page: string;
+		text: string;
+	}
+
+	let props: Props = $props();
+</script>
+
+<a href={resolve('/' + props.page)} class="index-link">{props.text}</a>
+
+<style>
+	.index-link {
+		background-color: #153d63;
+		box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
+		transition: background-color 0.2s;
+		border-radius: 20px;
+		width: 20%;
+		padding: 1%;
+		margin: 1% auto;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: #ffffff;
+		text-decoration: none;
+	}
+
+	.index-link:hover {
+		background-color: #4c94d8;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.index-link {
+			width: 90%;
+		}
+	}
+</style>
