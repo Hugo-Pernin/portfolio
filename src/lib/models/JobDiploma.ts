d5d7711dc6ae5eed1b3e828532e8db385cfd1157
Hugo-Pernin <hugopernin71@gmail.com>
2026-02-02 19:47:19 +0100

    Create JobDiploma model

---
+++ src/lib/models/JobDiploma.ts
@@ -0,0 +1,9 @@
+export default interface JobDiploma {
+	title: string;
+	etablishment: string;
+	city: string;
+	beginningYear: number;
+	endingYear: number;
+	shortDescription: string;
+	longDescription: string;
+}
