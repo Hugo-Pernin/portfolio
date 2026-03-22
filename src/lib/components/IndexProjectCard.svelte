9e65dce49083823307f806b6bc3045f5b0ab48f9
Hugo-Pernin <hugopernin71@gmail.com>
2026-03-22 14:23:47 +0100

    Create IndexProjectCard component

---
+++ src/lib/components/IndexProjectCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import LinkButton from './LinkButton.svelte';
+
+	interface Props {
+		title: string;
+		description: string;
+	}
+
+	let props: Props = $props();
+</script>
+
+<div class="index-project">
+	<h3>{props.title}</h3>
+	<p>{props.description}</p>
+	<LinkButton link={{ text: 'En savoir plus', href: 'projects#' + props.title }} newTab={false} />
+</div>
+
+<style>
+	.index-project {
+		border-radius: 10px;
+		padding: 0.96em;
+		margin: 3% 0;
+		box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
+		animation: fadein 1s;
+		width: 25%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.index-project p {
+		margin: 5% 0;
+		flex: 1;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.index-project {
+			width: auto;
+		}
+	}
+</style>
